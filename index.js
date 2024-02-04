14644fe7d2c41ded2852fc2b6557cf61fc0d94d7
itachimaru <depressive.suicidal.bm@gmail.com>
2024-02-04 13:13:37 +0100

    index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730, 440, 570, 1599340, 1408610, 2756830, 578080, 1172470, 1203220, 1938090, 2073850, 1449850];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
